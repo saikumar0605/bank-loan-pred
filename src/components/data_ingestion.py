c230154d6d87dc784d1e11cd18bc71cf1a37dcf0
saikumar0605 <123saikumarreddy@gmail.com>
2023-01-11 12:52:44 +0530

    data ingestion done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,16 +8,17 @@ from typing import Tuple
 logger = logging.getLogger(__name__)
 
 class DataIngestion:
-    def __init__(self, data_ingestion_config: DataIngestionConfig, mongo_op: MongoDBOperation):
+    def __init__(self, data_ingestion_config: DataIngestionConfig, mongo_op: MONGO_DB_URL):
         self.data_ingestion_config = data_ingestion_config
         self.mongo_op = mongo_op
+    
 
     def get_data_from_mongo(self) -> DataFrame:
         try:
             logger.info("Getting data from mongo using data_ingestion class")
-            data = self.mongo_op.read_from_mongo(self.data_ingestion_config.mongo_db_name,
-                                                 self.data_ingestion_config.mongo_collection_name)
+            data = self.mongo_op.read_from_mongo(self.data_ingestion_config.banking,
+                                                 self.data_ingestion_config.bankdf)
             return data
         except Exception as e:
             logger.error("Error while getting data from mongo")
-            raise 
+            raise e
